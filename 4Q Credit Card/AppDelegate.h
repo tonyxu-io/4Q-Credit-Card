74b19e97402b5e17bcd166d66e5e79b975af7cad
Yihan (Tony) Xu <yihan.xu@gmail.com>
2018-03-28 16:59:03 -0700

    Added responsive design.

---
+++ 4Q Credit Card/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  4Q Credit Card
+//
+//  Created by Tony Xu on 7/23/17.
+//  Copyright © 2017 Tony Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 4Q Credit Card/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  4Q Credit Card
+//
+//  Created by Tony Xu on 7/23/17.
+//  Copyright © 2017 Tony Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIButton *contactSupport;
+@property (weak, nonatomic) IBOutlet UIButton *dialHotline;
+@property (weak, nonatomic) IBOutlet UIButton *logout;
+
+@end
+
